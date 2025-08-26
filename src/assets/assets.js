4838411597a6999237bd7b9a0ef2fef91dce6037
sayantananotech <sayantan.pandey@anotechsolutions.com>
2025-08-26 12:13:24 +0530

    initial push

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -4,8 +4,7 @@ import order_icon from './order_icon.png'
 import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
-import { VITE_BACKEND_URL } from '../../env'
-export const url = VITE_BACKEND_URL
+export const url = import.meta.env.VITE_BACKEND_URL
 export const currency = '₹'
 
 export const assets ={
